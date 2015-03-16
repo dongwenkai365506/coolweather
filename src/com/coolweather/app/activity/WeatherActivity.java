0ea33c656b5ddb2d2f48e4748fd5f9a9da110875
dongwenkai365506 <369574045@qq.com>
2015-03-16 14:56:12 +0800

    修改程序图标和名称

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -48,7 +49,6 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		publishText = (TextView) findViewById(R.id.publish_text);
 		weatherDespText = (TextView) findViewById(R.id.weather_desp);
 		temp1Text = (TextView) findViewById(R.id.temp1);
-		temp2Text = (TextView) findViewById(R.id.temp2);
 		currentDateText = (TextView) findViewById(R.id.current_date);
 		String countyCode = getIntent().getStringExtra("county_code");
 		if(!TextUtils.isEmpty(countyCode)){
@@ -95,6 +95,7 @@ public class WeatherActivity extends Activity implements OnClickListener{
 	
 	private void queryWeatherInfo(String weatherCode){
 		String address = "http://m.weather.com.cn/data/"+weatherCode+".html";
+		Log.d("Weather", address);
 		queryFromServer(address, "weatherCode");
 	}
 	
@@ -104,6 +105,8 @@ public class WeatherActivity extends Activity implements OnClickListener{
 			@Override
 			public void onFinish(String response) {
 				// TODO Auto-generated method stub
+				Log.d("Weather", response);
+				
 				if("countyCode".equals(type)){
 					if(!TextUtils.isEmpty(response)){
 						String[] array = response.split("\\|");
@@ -144,13 +147,12 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		cityNameText.setText(prefs.getString("city_name", ""));
 		temp1Text.setText(prefs.getString("temp1", ""));
-		temp2Text.setText(prefs.getString("temp2", ""));
 		weatherDespText.setText(prefs.getString("weather_desp", ""));
 		publishText.setText("今天"+prefs.getString("publish_time", "")+"发布");
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
 		Intent intent = new Intent(this, AutoUpdateService.class);
-		startActivity(intent);
+		startService(intent);
 	}
 }
